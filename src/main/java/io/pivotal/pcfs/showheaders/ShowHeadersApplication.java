aee3b12699a2e3211bfc041e847311ec56391ff7
David Malone <dmalone@pivotal.io>
2015-09-02 15:16:06 -0400

    first commit

---
+++ src/main/java/io/pivotal/pcfs/showheaders/ShowHeadersApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.pcfs.showheaders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowHeadersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowHeadersApplication.class, args);
+    }
+}
